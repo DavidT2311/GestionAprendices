bd8de58d46c39446aee49b3f449f089d96c5dd5d
DavidT2311 <davidtorrez2311@gmail.com>
2024-08-28 22:46:58 -0500

    Se agregan las funciones actualizar programas y aprendices y se activa el LocalStorage

--- GestionAprendicesWeb/BackEnd/GestionAprendicesApp/Programas/views.py
+++ GestionAprendicesWeb/BackEnd/GestionAprendicesApp/Programas/views.py
@@ -5,6 +5,7 @@ def listar_programas(request):
     datos = obtener_programas()
     return render(request, "Programas/ListarProgramas.html", datos)
 
+
 def agregar_programa(request):
     if request.method == 'POST':
         numeroFicha = request.POST.get('Numero_Ficha')
@@ -16,4 +17,9 @@ def agregar_programa(request):
             return render(request, 'CuadrosAdicionales/Enviado.html', {"Redireccionamiento": "ListarProgramas"})
 
     return render(request, 'CuadrosAdicionales/NoEnviado.html', {"Redireccionamiento": "ListarProgramas"})
+
+
+def editar_programa(request):
+
+    return render(request, "")
 
--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let lista_aprendices = [];
 let lista_programas = [];
 
+const obtenerDatos = () => {
+        lista_aprendices = JSON.parse(localStorage.getItem("ListaAprendices"));
+        lista_programas = JSON.parse(localStorage.getItem("ListaProgramas"));
+};
+
 //Creando modelo aprendiz
 function aprendizModel(documento, nombre, apellido, telefono, direccion, programa) { //Modelo para los aprendices
     this.documento = documento;
@@ -30,9 +35,9 @@ const menuInicio = () => {
             3: Salir
             `
         ));
-            
+
         //Creacion de switch de programas y aprendices
-        switch(opcion) {
+        switch (opcion) {
             case 1:
                 llamarAprendices();
                 break;
@@ -40,6 +45,9 @@ const menuInicio = () => {
                 llamarProgramas();
                 break;
             case 3:
+                //Guardamos los datos
+                localStorage.setItem("ListaAprendices", JSON.stringify(lista_aprendices));
+                localStorage.setItem("ListaProgramas", JSON.stringify(lista_programas));
                 break;
             default:
                 alert("Digite una opcion correcta");
@@ -64,7 +72,7 @@ const llamarAprendices = () => {
         ));
 
         //Switch para elegir un metodo de los aprendices
-        switch(opcion) {
+        switch (opcion) {
             case 1:
                 ListarAprendices();
                 break;
@@ -101,7 +109,7 @@ const llamarProgramas = () => {
         ));
 
         //Switch para elegir un metodo de los programas
-        switch(opcion) {
+        switch (opcion) {
             case 1:
                 ListarProgramas();
                 break;
@@ -136,6 +144,7 @@ const ListarAprendices = () => {
     };
 };
 
+//CRUD para los aprendices
 const IngresarAprendiz = () => {
     //Guardar aprendices
     let cantidad = parseInt(prompt("Digite la cantidad de aprendices que desea guardar"));
@@ -148,7 +157,7 @@ const IngresarAprendiz = () => {
         let telefono = prompt("Digite el telefono");
         let direccion = prompt("Digite la direccion");
         let programa = parseInt(prompt("Digite el numero de ficha del programa"));
-        
+
         let nuevo_aprendiz = new aprendizModel(documento, nombre, apellido, telefono, direccion, programa);
         //Se guardan los cambios en los aprendices
 
@@ -171,7 +180,34 @@ const IngresarAprendiz = () => {
 };
 
 const ActualizarAprendiz = () => {
-    alert("Act aprendiz");
+    //Se pregunta al usuario que aprendiz desea actualizar
+    let actualizar_aprendices = parseInt(prompt("Ingrese el numero de documento del aprendiz por actualizar"));
+
+    let mensaje = "";
+
+    //Validacion si no hay aprendices
+    if (lista_aprendices.length == 0) {
+        mensaje = "Aun no hay aprendices"
+    }
+
+    let indice_aprendiz;
+    //Se busca el aprendiz que deseamos actualizar
+    lista_aprendices.forEach((elemento, indice) => {
+        //Validamos que el documento que ingreso el usuario este en la lista de aprendices
+        if (actualizar_aprendices == elemento.documento) {
+            mensaje = "Se ha encontrado el aprendiz";
+            indice_aprendiz = indice;
+        } else {
+            mensaje = "No se ha encontrado el aprendiz";
+        };
+    });
+
+    alert(mensaje);
+    //Validamos que se haya encontrado para ejecutar la funcion de actualizar aprendices
+    if (indice_aprendiz != null) {
+        menuActualizarAprendices(indice_aprendiz);
+    };
+
 };
 
 const EliminarAprendiz = () => {
@@ -195,10 +231,11 @@ const EliminarAprendiz = () => {
     } else {
         alert("No se ha encontrado el aprendiz");
     };
-    
+
 
 };
 
+//CRUD para los programas
 const ListarProgramas = () => {
     if (lista_programas.length == 0) {
         alert("No hay programas aun");
@@ -212,7 +249,7 @@ const ListarProgramas = () => {
 };
 
 const IngresarPrograma = () => {
-     //Guardar programas
+    //Guardar programas
     let cantidad = parseInt(prompt("Digite la cantidad de programas que desea guardar"));
 
     for (let i = 0; i < cantidad; i++) {
@@ -220,23 +257,53 @@ const IngresarPrograma = () => {
             numero_ficha: parseInt(prompt("Ingrese el numero de la ficha")),
             nombre_programa: prompt("Ingrese el nombre de la ficha")
         }
-        
-        for (let objeto of lista_programas) {
-            if (programa.numero_ficha == objeto.numero_ficha) {
-                alert("El programa ya existe");
-                break;
-            } else {
-                lista_programas.push(programa);
-                alert("Se ha ingresado el programa correctamente");
-                break;
+
+        if (!lista_programas.length == 0) {
+            for (let objeto of lista_programas) {
+                if (programa.numero_ficha == objeto.numero_ficha) {
+                    alert("El programa ya existe");
+                    break;
+                } else {
+                    lista_programas.push(programa);
+                    alert("Se ha ingresado el programa correctamente");
+                    break;
+                };
             };
+        } else {
+            lista_programas.push(programa);
+            alert("Se ha ingresado el programa correctamente");
         };
     };
 };
 
 const ActualizarPrograma = () => {
-    
-    
+    //Se pregunta al usuario que programa desea actualizar
+    let actualizar_programa = parseInt(prompt("Ingrese el numero de ficha por actualizar"));
+
+    let mensaje = "";
+
+    //Validacion si no hay programas
+    if (lista_aprendices.length == 0) {
+        mensaje = "Aun no hay programas"
+    }
+
+    let indice_programa;
+    //Se busca el programa que deseamos actualizar
+    lista_programas.forEach((elemento, indice) => {
+        //Validamos que el numero de ficha que ingreso el usuario este en la lista de programas
+        if (actualizar_programa == elemento.numero_ficha) {
+            mensaje = "Se ha encontrado el programa";
+            indice_programa = indice;
+        } else {
+            mensaje = "No se ha encontrado el programa";
+        };
+    });
+
+    alert(mensaje);
+    //Validamos que se haya encontrado para ejecutar la funcion de actualizar programa
+    if (indice_programa != null) {
+        menuActualizarPrograma(indice_programa);
+    };
 };
 
 const EliminarPrograma = () => {
@@ -262,5 +329,134 @@ const EliminarPrograma = () => {
     };
 };
 
+//Menu de actualizar programas
+const menuActualizarPrograma = (indice_programa) => {
+    let opcion;
+    do {
+        //Menu para elegir que campo desea actualizar 
+        opcion = parseInt(prompt(`Eliga una opcion
+            1: Nombre del programa
+            2: Salir
+            `));
 
+        //Creacion de switch para actualizar nombre
+        switch (opcion) {
+            case 1:
+                actualizarNombrePrograma(indice_programa);
+                opcion = 2;
+                break;
+            case 2:
+                break;
+            default:
+                alert("Digite una opcion correcta");
+        };
+
+    } while (opcion != 2);
+};
+
+//Funcion para modificar el nombre del programa
+const actualizarNombrePrograma = (indice_programa) => {
+    let nuevo_nombre_programa = prompt("Ingrese el nuevo nombre del programa");
+    //Validacion para verificar que el nombre por actualizar del programa tenga contenido
+    while (nuevo_nombre_programa == "") {
+        alert("Digite correctamente el nombre del programa");
+        nuevo_nombre_programa = prompt("Ingrese el nuevo nombre del programa");
+    };
+
+    lista_programas[indice_programa].nombre_programa = nuevo_nombre_programa;
+    alert("Se ha actualizado el programa correctamente");
+};
+
+//Menu para actualizar aprendices
+const menuActualizarAprendices = (indice_aprendiz) => {
+
+    let opcion;
+    do {
+        //Menu para elegir que campo desea actualizar 
+        opcion = parseInt(prompt(`Eliga una opcion
+            1: Nombre 
+            2: Apellido
+            3: Telefono
+            4: Direccion
+            5: Salir
+            `));
+
+        //Creacion de switch para actualizar los campos de los aprendices
+        switch (opcion) {
+            case 1:
+                actualizarNombreAprendiz(indice_aprendiz);
+                break;
+            case 2:
+                actualizarApellidoAprendiz(indice_aprendiz);
+                break;
+            case 3:
+                actualizarTelefonoAprendiz(indice_aprendiz);
+                break;
+            case 4:
+                actualizarDireccionAprendiz(indice_aprendiz);
+                break;
+            case 5:
+                break;
+            default:
+                alert("Digite una opcion correcta");
+        };
+
+    } while (opcion != 5);
+
+};
+
+//Funcion para modificar el nombre del aprendiz
+const actualizarNombreAprendiz = (indice_aprendiz) => { 
+    let nuevo_nombre_aprendiz = prompt("Ingrese el nuevo nombre del aprendiz");
+    //Validacion para verificar que el nombre por actualizar del aprendiz tenga contenido
+    while (nuevo_nombre_aprendiz == "") {
+        alert("Digite correctamente el nombre del aprendiz");
+        nuevo_nombre_aprendiz = prompt("Ingrese el nuevo nombre del aprendiz");
+    };
+
+    lista_aprendices[indice_aprendiz].nombre = nuevo_nombre_aprendiz;
+    alert("Se ha actualizado el nombre correctamente");
+};
+
+//Funcion para modificar el apellido del aprendiz
+const actualizarApellidoAprendiz = (indice_aprendiz) => { 
+    let nuevo_apellido_aprendiz = prompt("Ingrese el nuevo apellido del aprendiz");
+    //Validacion para verificar que el apellido por actualizar del aprendiz tenga contenido
+    while (nuevo_apellido_aprendiz == "") {
+        alert("Digite correctamente el apellido del aprendiz");
+        nuevo_apellido_aprendiz = prompt("Ingrese el nuevo apellido del aprendiz");
+    };
+
+    lista_aprendices[indice_aprendiz].apellido = nuevo_apellido_aprendiz;
+    alert("Se ha actualizado el apellido correctamente");
+};
+
+//Funcion para modificar el telefono del aprendiz
+const actualizarTelefonoAprendiz = (indice_aprendiz) => {
+    let nuevo_telefono_aprendiz = parseInt(prompt("Ingrese el nuevo apellido del aprendiz"));
+    //Validacion para verificar que el telefono por actualizar del aprendiz tenga contenido
+    while (nuevo_telefono_aprendiz == "") {
+        alert("Digite correctamente el telefono del aprendiz");
+        nuevo_telefono_aprendiz = parseInt(prompt("Ingrese el nuevo apellido del aprendiz"));
+    };
+
+    lista_aprendices[indice_aprendiz].telefono = nuevo_telefono_aprendiz;
+    alert("Se ha actualizado el telefono correctamente");
+};
+
+ //Funcion para modificar el direccion del aprendiz
+const actualizarDireccionAprendiz = (indice_aprendiz) => { 
+    let nueva_direccion_aprendiz = prompt("Ingrese la nueva direccion del aprendiz");
+    //Validacion para verificar que el direccion por actualizar del aprendiz tenga contenido
+    while (nueva_direccion_aprendiz == "") {
+        alert("Digite correctamente la direccion del aprendiz");
+        nueva_direccion_aprendiz = prompt("Ingrese la nueva direccion del aprendiz");
+    };
+
+    lista_aprendices[indice_aprendiz].direccion = nueva_direccion_aprendiz;
+    alert("Se ha actualizado la direccion correctamente");
+};
+
+obtenerDatos();
 menuInicio();
+
